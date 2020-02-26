81b7eb17eca4354095ccd99d8151d48dc8bb0004
thto <thiagohtoliva@gmail.com>
2020-02-26 23:19:14 +0100

    at least I'm not getting any errors now

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,12 +1,34 @@
+//import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function transform(offset) {
+    const cos = Math.cos(offset);
+    const sin = Math.sin(offset);
+    return { transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)` };
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Create a React component
+class App extends React.Component {
+    state = { styleOne: {}, styleTwo: {} };
+
+    onMouseMove = (event) => {
+        this.setState({
+            styleOne: transform(-event.clientX / event.clientY),
+            styleTwo: transform(event.clientX / event.clientY)
+        })
+    }
+
+    render() {
+        return <div onMouseMove={this.onMouseMove}>
+            <div className="panel" style={this.state.styleOne} />
+            <div className="panel" style={this.state.styleTwo} />
+        </div>
+    }
+}
+
+//Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
