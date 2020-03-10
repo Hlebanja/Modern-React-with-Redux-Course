2de4d6a5be5f5e17428cd370c0904ef3798407a9
thto <thiagohtoliva@gmail.com>
2020-03-10 10:08:14 +0100

    add untracked firstapp file

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -2,30 +2,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function transform(offset) {
-    const cos = Math.cos(offset);
-    const sin = Math.sin(offset);
-    return { transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)` };
+
+function getButtonText() {
+    return "click me!";
 }
 
 //Create a React component
-class App extends React.Component {
-    state = { styleOne: {}, styleTwo: {} };
-
-    onMouseMove = (event) => {
-        this.setState({
-            styleOne: transform(-event.clientX / event.clientY),
-            styleTwo: transform(event.clientX / event.clientY)
-        })
-    }
-
-    render() {
-        return <div onMouseMove={this.onMouseMove}>
-            <div className="panel" style={this.state.styleOne} />
-            <div className="panel" style={this.state.styleTwo} />
+const App = () => {
+    return (
+        <div>
+            <div>Hi there!</div>
+            <div>
+                <label className="label" htmlFor="name">
+                    Enter name:
+            </label>
+                <input id="name" type="text" />
+                <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                    {getButtonText()}
+                </button>
+            </div>
         </div>
-    }
-}
+    );
+};
 
 //Take the React component and show it on the screen
 ReactDOM.render(
