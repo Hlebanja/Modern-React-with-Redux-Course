2cc7681b1a8b22cfb6672178ac506da90bc4a7ef
thto <thiagohtoliva@gmail.com>
2020-03-10 10:45:39 +0100

    creat first component

--- components/src/index.js
+++ components/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
+        // Componenets are the only exception to the usage
+        // of {}. Even though it is a JS variable,
+        // we treat it as if it was a JSX tag.
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail /> 
         </div>
     );
 };
