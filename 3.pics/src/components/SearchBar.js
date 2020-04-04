713892c1c39fd0c42b8e4922c78dab6e11d5aaba
thto <thiagohtoliva@gmail.com>
2020-04-04 16:13:19 +0200

    improve SearchBar

--- 3.pics/src/components/SearchBar.js
+++ 3.pics/src/components/SearchBar.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: '' };
+    state = { term: 'search here' };
     //"uncontrolled" synthax
     // onInputChange(event) {
     //     console.log(event.target.value);
-    // }
+    // } 
+    // "uncontrolled" synthax: onChange={this.onInputChange}
+
+    onFormSubmit(event) {
+         // console.log(this.state.term);
+        event.preventDefault();
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         {/* we do not put a set of parentheses when we 
                         pass a callback function to an event hangler like onChange. */}
-                        {/* "uncontrolled" synthax: onChange={this.onInputChange} */}
-                        <input type="text"
+
+                        <input
+                            type="text"
                             value={this.state.term}
-                            onChange={e => this.setState({ term: e.target.value })} />
+                            onChange={e => this.setState({ term: e.target.value.toUpperCase() })}
+                        />
                     </div>
                 </form>
             </div>
